e1aba1f5c2f5a07085a25be4148b5c4eee4538b7
amlzq <lzqjiujiang@gmail.com>
2019-10-25 18:52:51 +0800

    change PopupWindowDemo

--- platform/src/main/java/com/amlzq/android/viewer/platform/PopupWindowDemoActivity.java
+++ platform/src/main/java/com/amlzq/android/viewer/platform/PopupWindowDemoActivity.java
@@ -2,12 +2,25 @@ package com.amlzq.android.viewer.platform;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
+/**
+ * PopupWindow样例
+ */
 public class PopupWindowDemoActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup_window_demo);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+
+        });
+        PopupWindow popupWindow = new PopupWindow();
+        popupWindow.showAsDropDown(button);
     }
+
 }
